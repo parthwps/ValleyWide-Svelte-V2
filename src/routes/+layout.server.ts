08697452e669a21d00d2236b894db173a773e27a
parthwps <parth@netdevs.net>
2024-05-08 01:36:16 +0530

    Update +layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,21 +1,40 @@
 import axios from 'axios';
 import { STRAPI_API } from '$env/static/private';
 
-export async function load(){
+export async function load() {
+    try {
+        // Send multiple requests concurrently
+        const [menuResponse, siteSettingsResponse] = await Promise.all([
+            fetchMenu(),
+            fetchSiteSettings()
+        ]);
+
+        return {
+            data: menuResponse.data,
+            fallback: siteSettingsResponse.data
+        };
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return {
+            error: 'Failed to fetch data'
+        };
+    }
+}
+
+async function fetchMenu() {
     const url = 'https://vwapi.netdevs.net/api/menus/1?nested&populate=*';
     const headers = {
-        Authorization: 'Bearer ' + STRAPI_API
+        Authorization: `Bearer ${STRAPI_API}`
     };
-    const urlSiteSettings = 'https://vwapi.netdevs.net/api/site-setting?populate=deep,3';
-    const siteSettings = await axios.get(urlSiteSettings, { headers }); 
+    const response = await axios.get(url, { headers });
+    return response;
+}
 
-    try {
-        const response = await axios.get(url, { headers });
-        return {
-            data: response.data,
-            fallback: siteSettings.data
-        }
-    } catch (error) {
-        console.log(error);
-    }    
+async function fetchSiteSettings() {
+    const url = 'https://vwapi.netdevs.net/api/site-setting?populate=deep,3';
+    const headers = {
+        Authorization: `Bearer ${STRAPI_API}`
+    };
+    const response = await axios.get(url, { headers });
+    return response;
 }
